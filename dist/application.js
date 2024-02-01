a4fe6660e783e31b6b5ae43fd304efff0043e223
Hori, Daisuke <hori.daisuke@polymorphics.jp>
2024-02-01 13:53:46 +0900

    npm run build

--- dist/application.js
+++ dist/application.js
@@ -3,16 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createApplicationCaller = exports.createApplication = void 0;
 const middleware_stack_1 = require("./middleware-stack");
 const createApplication = (application) => {
-    var _a, _b;
+    var _a, _b, _c, _d;
     const stack = (_a = application === null || application === void 0 ? void 0 : application.stack) !== null && _a !== void 0 ? _a : (0, middleware_stack_1.createMiddlewareStack)();
-    const caller = (_b = application === null || application === void 0 ? void 0 : application.caller) !== null && _b !== void 0 ? _b : (0, exports.createApplicationCaller)();
+    const initialize = (_b = application === null || application === void 0 ? void 0 : application.initialize) !== null && _b !== void 0 ? _b : (0, exports.createApplicationCaller)();
+    const handle = (_c = application === null || application === void 0 ? void 0 : application.handle) !== null && _c !== void 0 ? _c : (0, exports.createApplicationCaller)();
+    const finalize = (_d = application === null || application === void 0 ? void 0 : application.finalize) !== null && _d !== void 0 ? _d : (0, exports.createApplicationCaller)();
     return Object.assign({
         stack,
-        caller,
+        initialize,
+        handle,
+        finalize,
         use: (middleware) => stack.add(middleware),
-        ready: () => caller(stack.initializer().reverse()),
-        process: () => caller(stack.handler().reverse()),
-        close: () => caller(stack.finalizer().reverse()),
+        ready: () => initialize(stack.initializer().reverse()),
+        process: () => handle(stack.handler().reverse()),
+        close: () => finalize(stack.finalizer().reverse()),
     }, application);
 };
 exports.createApplication = createApplication;
--- types/application.d.ts
+++ types/application.d.ts
@@ -8,16 +8,20 @@ export declare const createApplication: (application?: {
     ready?: ((option?: () => any) => void) | undefined;
     process?: ((option?: () => any) => void) | undefined;
     close?: ((option?: () => any) => void) | undefined;
-    caller?: ((chain: ApplicationCallerChain) => void) | undefined;
+    initialize?: ((chain: ApplicationCallerChain<MiddlewareInitializer<any>>) => void) | undefined;
+    handle?: ((chain: ApplicationCallerChain<MiddlewareHandler<any, any>>) => void) | undefined;
+    finalize?: ((chain: ApplicationCallerChain<MiddlewareFinalizer<any>>) => void) | undefined;
 } | undefined) => Application<Middleware<MiddlewareInitializer<any>, MiddlewareHandler<any, any>, MiddlewareFinalizer<any>>, any, any, any>;
-export declare const createApplicationCaller: (caller?: (chain: ApplicationCallerChain) => void) => (chain: ApplicationCallerChain) => void;
+export declare const createApplicationCaller: (caller?: (chain: ApplicationCallerChain<any>) => void) => (chain: ApplicationCallerChain<any>) => void;
 export interface Application<M extends Middleware<MiddlewareInitializer<any>, MiddlewareHandler<any, any>, MiddlewareFinalizer<any>>, ReadyOption, ProcessOption, CloseOption> {
     stack: MiddlewareStack<M>;
     use: (middleware: M) => number;
     ready: (option?: () => ReadyOption) => void;
     process: (option?: () => ProcessOption) => void;
     close: (option?: () => CloseOption) => void;
-    caller: (chain: ApplicationCallerChain) => void;
+    initialize: (chain: ApplicationCallerChain<MiddlewareInitializer<any>>) => void;
+    handle: (chain: ApplicationCallerChain<MiddlewareHandler<any, any>>) => void;
+    finalize: (chain: ApplicationCallerChain<MiddlewareFinalizer<any>>) => void;
 }
-export interface ApplicationCallerChain extends Array<MiddlewareInitializer<any> | MiddlewareHandler<any, any> | MiddlewareFinalizer<any>> {
+export interface ApplicationCallerChain<T> extends Array<T> {
 }
